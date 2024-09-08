f17beeb4a66e339f2cb7fa05641898bdbf25059a
c4dd3 <danielowu1012@gmail.com>
2024-09-07 18:19:06 -0600

    Initial commit

---
+++ A_SANDBOX_SCRIPT_1.js
@@ -0,0 +1,6 @@
+function processTexts(){
+    const text1 = document.getElementById("idtext1").value;
+    const text2 = document.getElementById("idtext2").value;
+    console.log("TEXT1: " + text1);
+    console.log("TEXT2: " + text2);
+}
