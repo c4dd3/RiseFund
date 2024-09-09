ee3d1ade2d4d7801f6587a21148298522fa44521
niparra21 <niparra@estudiantec.cr>
2024-09-08 22:50:58 -0600

    22.50

--- Public/UserFiles/CustomerSupport_Script_1.js
+++ Public/UserFiles/CustomerSupport_Script_1.js
@@ -6,3 +6,20 @@ function toggleDropdown() {
         dropdownMenu.style.display = 'block';
     }
 }
+
+function toggleFAQ(button) {
+    const answer = button.parentElement.nextElementSibling;
+    const isAnswerVisible = answer.style.display === "block";
+    
+    if (isAnswerVisible) {
+        // Ocultar la respuesta
+        answer.style.display = "none";
+        // Cambiar el botón a +
+        button.innerText = "+";
+    } else {
+        // Mostrar la respuesta
+        answer.style.display = "block";
+        // Cambiar el botón a -
+        button.innerText = "-";
+    }
+}
