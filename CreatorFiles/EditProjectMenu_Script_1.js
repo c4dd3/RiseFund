ddcab7a41d54bbc2f4ff67652b56967136f8896b
Francisco Kuo Liu <130787535+franciscokuoliu@users.noreply.github.com>
2024-09-08 20:56:30 -0600

    edit project
    falta conectar con base de datos

---
+++ CreatorFiles/EditProjectMenu_Script_1.js
@@ -0,0 +1,55 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById('dropdownMenu');
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+}
+
+function showConfirmation() {
+    const projectID = document.getElementById("project-id").value;
+    const startDate = document.getElementById("start-date").value;
+    const goal = document.getElementById("goal").value;
+    const completionDate = document.getElementById("completion-date").value;
+    const status = document.getElementById("status").value;
+    const contact = document.getElementById("contact").value;
+    const depositMethod = document.getElementById("deposit-method").value;
+    const expirationDate = document.getElementById("expiration").value;
+    const accountNumber = document.getElementById("account-number").value;
+
+
+    if (!goal || !contact || !accountNumber) {
+        alert("Please fill in all the required fields before confirming.");
+        return;
+    }
+
+    const confirmed = confirm(`
+    Please verify the project details:
+    - Project ID: ${projectID}
+    - Start Date: ${startDate}
+    - Contribution Goal: ${goal}
+    - Expected Completion: ${completionDate}
+    - Status: ${status}
+    - Project Contact: ${contact}
+    - Deposit Method: ${depositMethod}
+    - Expiration Date: ${expirationDate}
+    - Account Number: ${accountNumber}
+
+    Confirm changes?`);
+
+    if (confirmed) {
+        alert("Project updated!");
+    }
+}
+
+function updateImage(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        document.getElementById('uploaded-image').src = e.target.result;
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
