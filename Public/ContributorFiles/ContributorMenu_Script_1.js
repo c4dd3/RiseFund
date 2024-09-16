aa4ed196a133387275709362c44e8b5d7a29591a
c4dd3 <danielowu1012@gmail.com>
2024-09-15 21:21:48 -0600

    21.21

--- Public/ContributorFiles/ContributorMenu_Script_1.js
+++ Public/ContributorFiles/ContributorMenu_Script_1.js
@@ -1,4 +1,4 @@
-var raised = 10000;
+var raised = 0;
 var progress = 100;
 
 function toggleDropdown() {
@@ -29,11 +29,11 @@ document.addEventListener('DOMContentLoaded', async () => {
         const projectsDisplay = document.querySelector('.ProjectsDisplay');
         projectsDisplay.innerHTML = '';
         projects.forEach(project => {
-            if(project.Status == 1 && project.Collected <= raised){ // Los bloqueados o finalizados no
+            if(project.Status == 1 && project.Collected >= raised){ // Los bloqueados o finalizados no
                 let number = (project.Collected * 100) / project.ContributionGoal;
                 let limitedDecimals = number.toFixed(2);  
                 const percentage = parseFloat(limitedDecimals); 
-                if (percentage <= progress) {
+                if (percentage >= progress) {
                 const projectPanel = `
                     <div class="InfoPanel">
                         <img class="Portada" src="ProjectImage.jpg" alt="Project Image">
@@ -64,7 +64,55 @@ document.addEventListener('DOMContentLoaded', async () => {
     if (document.querySelector('.ProjectsDisplay')) {
         loadProjects();
     }
+    try {
+        const adminLink = document.getElementById('adminLink');
+        adminLink.addEventListener('click', (event) => {
+            verifyAdminUser();
+        });
+    } catch (error) {
+        // Manejo de errores en caso de problemas con la solicitud
+        console.error('Error at loading element:', error);
+    }
 });
+
+
+async function verifyAdminUser() {
+    const UserID = sessionStorage.getItem('userID');
+    const isUserAdmin = await checkIfUserIsAdmin(UserID);
+    if(isUserAdmin){
+        window.location.href = "../AdminFiles/AdministratorMenu.html";
+    } else {
+        alert('You do not have admin privileges');
+    }
+}
+
+async function checkIfUserIsAdmin(userID) {
+    try {
+        // Hacer la solicitud para obtener la lista de administradores
+        const response = await fetch('/GetAdminList');
+        
+        if (!response.ok) {
+            throw new Error('Error al obtener la lista de administradores');
+        }
+        
+        // Convertir la respuesta en un array de administradores
+        const admins = await response.json();
+        console.log(admins);
+        // Verificar si algún administrador tiene el mismo ID que el userID
+        const isAdmin = admins.some(admin => admin.UserID == userID);
+        
+        if (isAdmin) {
+            console.log(`El usuario con ID ${userID} es administrador.`);
+        } else {
+            console.log(`El usuario con ID ${userID} no es administrador.`);
+        }
+        
+        return isAdmin;
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
+}
 
 function applyFilters() {
     
--- Public/CreatorFiles/NewProjectMenu_Script_1.js
+++ Public/CreatorFiles/NewProjectMenu_Script_1.js
@@ -169,4 +169,55 @@ async function setLastProjectID() {
     }
 }
 
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const adminLink = document.getElementById('adminLink');
+        adminLink.addEventListener('click', (event) => {
+            verifyAdminUser();
+        });
+    } catch (error) {
+        // Manejo de errores en caso de problemas con la solicitud
+        console.error('Error at loading element:', error);
+    }
+});
+
+
+async function verifyAdminUser() {
+    const UserID = sessionStorage.getItem('userID');
+    const isUserAdmin = await checkIfUserIsAdmin(UserID);
+    if(isUserAdmin){
+        window.location.href = "../AdminFiles/AdministratorMenu.html";
+    } else {
+        alert('You do not have admin privileges');
+    }
+}
+
+async function checkIfUserIsAdmin(userID) {
+    try {
+        // Hacer la solicitud para obtener la lista de administradores
+        const response = await fetch('/GetAdminList');
+        
+        if (!response.ok) {
+            throw new Error('Error al obtener la lista de administradores');
+        }
+        
+        // Convertir la respuesta en un array de administradores
+        const admins = await response.json();
+        console.log(admins);
+        // Verificar si algún administrador tiene el mismo ID que el userID
+        const isAdmin = admins.some(admin => admin.UserID == userID);
+        
+        if (isAdmin) {
+            console.log(`El usuario con ID ${userID} es administrador.`);
+        } else {
+            console.log(`El usuario con ID ${userID} no es administrador.`);
+        }
+        
+        return isAdmin;
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
+}
+
 window.onload = setLastProjectID;
